a4705320bb4b5e03d744e16fd5446509b1ff4df1
Anks <62417515+ankita-y@users.noreply.github.com>
2021-04-25 18:12:22 +0530

    Array Concatenate

---
+++ Numpy_Concatenate_1.py
@@ -0,0 +1,19 @@
+# Two or more arrays can be concatenated together using the concatenate function with a tuple of the arrays to be joined:
+import numpy as np
+
+n,m,p = map(int,input().split())
+L1 = []
+L2 = []
+for i in range(n):
+    a = []
+    a = list(int(j) for j in input().split())
+    L1.append(a)
+for i in range(m):
+    a = []
+    a = list(int(j) for j in input().split())
+    L2.append(a)
+
+print(L1)
+arr1 = np.array(L1)
+arr2 = np.array(L2)
+print(np.concatenate((arr1,arr2)))
